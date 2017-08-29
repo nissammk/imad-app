74d6acbf3a2e2182a3589dd1f2c5fe05177428a7
nissammk <31043200+nissammk@users.noreply.github.com>
2017-08-29 21:05:41 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles= {
-`article-one = {
+var articles = {
+    `article-one` : {
     title: 'Article one | Nissam ',
     heading:'Article one',
     date : '29 Aug 2017',
@@ -21,7 +21,7 @@ var articles= {
                     This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
                 </p>`
 },
-`article-two = { 
+    `article-two` : { 
     title: 'Article one | Nissam ',
     heading:'Article Two',
     date : '29 Juy 2017',
@@ -30,7 +30,7 @@ var articles= {
                     This is the content for my second article.
     </p>`
     },
-`article-three ={
+    `article-three`:{
     title: 'Article one | Nissam ',
     heading:'Article Three',
     date : '29 June 2017',
@@ -38,7 +38,7 @@ var articles= {
     `<p>
                     This is the content for my third article.
     </p>`
-};
+}
 };
 
 
