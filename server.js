897bb78b8be29f6acc6ad5516d49692e183a7506
nissammk <31043200+nissammk@users.noreply.github.com>
2017-08-29 11:28:24 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/article-One',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 app.get('/article-Two',function(req,res) {
-    res.send ('Article-Two requested and wil be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
 });
 app.get('/article-Three',function(req,res) {
     res.send ('Article-Three requested and wil be served here');
